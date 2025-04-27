2ae0a014a00973f66bfdf50344fbd2ccf4ce5624
Antonio Ishac <amtonioishac@gmail.com>
2025-04-27 12:39:16 -0300

    Iniciando o projeto do curso de especialista Java

---
+++ src/main/java/br/com/aishac/especialista/Application.java
@@ -0,0 +1,13 @@
+package br.com.aishac.especialista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aishac/especialista/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aishac.especialista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
